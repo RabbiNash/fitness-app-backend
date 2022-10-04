b8f69bafcbce8da926c3bf8d9a22c013be8f55f5
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-04 22:34:15 +0200

    feat :adding contents of the app file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import * as  exprss from "express"
 
 class App{
+    public express :exprss.Application
 
-    
+    constructor(){
+
+    }
+
+    private setMiddleware() :void{
+
+    }
+
+    private setRoutes() :void {
+
+    }
+
+    private async connectToDB() : Promise<void> {
+
+    }
+
+    private catchError() :void{
+
+    }
 }

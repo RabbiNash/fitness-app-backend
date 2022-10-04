ceb7c52fdf09cde87692d32e503233f729a5c8df
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-04 23:18:50 +0200

    feat :configuration of the app file done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import * as  exprss from "express"
+import cors from  "cors"
+import morgan from "morgan"
 
 class App{
     public express :exprss.Application
 
     constructor(){
+        this.express = exprss()
+        this.setMiddleware()
+        this.setRoutes()
+        this.connectToDB()
+        this.catchError()
 
     }
 
     private setMiddleware() :void{
+        this.express.use(cors())
+        this.express.use(exprss.json())
+        this.express.use(morgan("dev"))
 
     }
 
@@ -23,3 +33,7 @@ class App{
 
     }
 }
+
+
+
+export default new App().express;

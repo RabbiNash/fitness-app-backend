18a01703e3ea7556bfda29295432f50f5259e7b1
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-05 14:04:48 +0200

    feat :connection to db successfully

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,37 @@
-import  express from "express"
-import cors from  "cors"
-import morgan from "morgan"
-import  apiV1 from "../src/api/index"
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import apiV1 from "../src/api/index";
+import { PgDbHelper } from "./config/pgDb.config";
 
-class App{
-    public express :express.Application
+class App {
+  public express: express.Application;
 
-    constructor(){
-        this.express = express()
-        this.setMiddleware()
-        this.setRoutes()
-        this.connectToDB()
-        this.catchError()
+  constructor() {
+    this.express = express();
+    this.setMiddleware();
+    this.setRoutes();
+    this.connectToDB();
+    this.catchError();
+  }
 
-    }
+  private setMiddleware(): void {
+    this.express.use(cors());
+    this.express.use(express.json());
+    this.express.use(morgan("dev"));
+  }
 
-    private setMiddleware() :void{
-        this.express.use(cors())
-        this.express.use(express.json())
-        this.express.use(morgan("dev"))
+  private setRoutes(): void {
+    this.express.use("/api/v1", apiV1);
+  }
 
-    }
+  private async connectToDB(): Promise<void> {
+    (await PgDbHelper.connect()).authenticate();
 
-    private setRoutes() :void {
-        this.express.use("/api/v1",apiV1)
+    console.log("database connection");
+  }
 
-    }
-
-    private async connectToDB() : Promise<void> {
-
-    }
-
-    private catchError() :void{
-
-    }
+  private catchError(): void {}
 }
-
-
 
 export default new App().express;
---
+++ src/config/pgDb.config.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+
+export class PgDbHelper {
+
+    public static client:Sequelize
+
+    public static async connect () :Promise<Sequelize> {
+
+        try {
+            this.client = new Sequelize('dashboard', 'postgres', "1311", {
+                host: 'localhost',
+                dialect: "postgres"
+              });
+
+              return this.client
+            
+        } catch (error) { 
+            throw error
+            
+        }
+    }
+
+}
+

d2d10f7b9853379d37e881ac0dff951a91731dc8
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-21 10:59:34 +0200

    get :tests done

--- tests/resources/resources.ts
+++ tests/resources/resources.ts
@@ -34,6 +34,20 @@ let exerciseInput = new ExerciseClass(
   "cf5631e5-d5dc-4f52-9a07-de6a63fbebdc",
   "6c37b303-ac2f-475a-9b14-f7308ccf77a8"
 );
+
+ const exerciseInstance =  {
+  "Id": "e941f7d1-c65c-4995-a53f-40a0f56c4a6a",
+  "exerciseName": "blackmhofu",
+  "weight": 67,
+  "reps": 4,
+  "duration": 5,
+  "distance": 5,
+  "exerciseNotes": "Here are some notes about this workout type..",
+  "createdAt": "2022-10-20T14:18:55.290Z",
+  "updatedAt": "2022-10-20T14:18:55.290Z",
+  "userId": "9bf78f57-91bd-45a9-87ab-2b822bdb3395",
+  "workoutTypeId": "d204ec21-9217-4eeb-bbbe-9db39b46d505"
+}
 const exercisePayload = {
   success: true,
   msg: "exercise was successfully created !! ",
@@ -56,6 +70,7 @@ export {
      app, 
      userPayLoad,
       userInput, 
+      exerciseInstance,
       User, 
       request, 
       exerciseInput,
--- tests/userTest/exercise.test.ts
+++ tests/userTest/exercise.test.ts
@@ -48,11 +48,73 @@ describe("Exersise controllers  Tests", () => {
       
       expect(msg).toBe("The following params are required");
       expect(missingParams).toBeInstanceOf(Array)
-  
-
- 
     });
   });
 
+  describe("When the service throws", () => {
+    //@ts-ignore
+    let exerciseServiceMock = jest
+      .spyOn(exerciseservice, "addexerciseRepository").mockRejectedValueOnce("I cant move !!!!")
 
-});
+    test("should be false", async () => {
+      let exerciseCreationRes = await request(app)
+        .post("/api/v1/workout/exercise")
+        .send({
+           ...exerciseInput
+          
+        });
+      const { success, msg } = await exerciseCreationRes.body;
+      /**
+       * Body to be implemented
+       */
+      
+    });
+  });
+
+  /**
+   * test case  2 :
+   * 1.get an existing exercise 
+   * 2.get a non existing 
+   */
+
+  describe("Fetching Exercise !!!! " , () =>{
+
+    describe("given the exerciseId" , () =>{
+           //@ts-ignore
+    let exerciseServiceMock = jest.spyOn(exerciseservice, "getexerciseRepository").mockResolvedValueOnce(exerciseInput)
+
+    test("should have success , msg,exercise  fields in body", async () => {
+        let exerciseCreationRes = await request(app)
+          .get("/api/v1/workout/exercise/6c37b303-ac2f-475a-9b14-f7308ccf77a8")
+          .send({
+             ...exerciseInput
+            
+          });
+        const {  exercise } = await exerciseCreationRes.body;
+
+        expect(exercise).toBeTruthy
+       
+      });
+    })
+
+    test("with invalid id  should return false", async () => {
+        let exerciseCreationRes = await request(app)
+          .get("/api/v1/workout/exercise/6c37b303-ac2f-475a-9b14-f7308ccf77a8")
+          .send({
+             ...exerciseInput
+            
+          });
+        const { success} = await exerciseCreationRes.body;
+
+        expect(success).toBeFalsy
+
+       
+      });
+    })
+
+    
+    
+  })
+
+
+

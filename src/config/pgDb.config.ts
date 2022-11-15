3f8d0c886f668d8cc911893fb1458c4882a2b465
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-15 23:14:58 +0200

    feat :docker file configuration

--- src/config/pgDb.config.ts
+++ src/config/pgDb.config.ts
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 
-let postgresDdClient = new Sequelize("fitness_db", process.env.DB_USER!, process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+let postgresDdClient = new Sequelize(
+  "fitness_db",
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 export default postgresDdClient;

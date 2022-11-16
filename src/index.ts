f4caffcd328922f290ee58712396ec6381ced05f
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-16 13:13:14 +0200

    feat :trigger deployment

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import app from "./app";
 import swaggerDocs from "./helpers/swaggerDocs";
 
 //@ts-ignore
-const PORT: number = process.env.PORT|| 4000;
+const PORT: number = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port : ${PORT}`);
   swaggerDocs(app, PORT);

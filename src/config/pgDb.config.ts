12d402d1867080cc46c901fef5a2c4fe4b6421ca
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-26 16:02:32 +0200

    chor : adde appspec file

--- src/config/pgDb.config.ts
+++ src/config/pgDb.config.ts
@@ -1,14 +1,32 @@
 import { Sequelize } from "sequelize";
 
-let postgresDdClient = new Sequelize(
-  "fitness_db",
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD,
+// let postgresDdClient = new Sequelize(
+//   "fitness_db",
+//   process.env.DB_USER!,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+const postgresDdClient = new Sequelize(
+  process.env.POSTGRESQL_DB!,
+  process.env.POSTGRESQL_DB_USER!,
+  process.env.POSTGRESQL_DB_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.POSTGRESQL_DB_HOST,
     dialect: "postgres",
-    logging: false,
+    // declaring pool is optional
+    // pool: {
+    //   max: dbConfig.pool.max,
+    //   min: dbConfig.pool.min,
+    //   acquire: dbConfig.pool.acquire,
+    //   idle: dbConfig.pool.idle
+    // }
   }
 );
+
 
 export default postgresDdClient;

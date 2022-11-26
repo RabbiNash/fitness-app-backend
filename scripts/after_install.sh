daa1a8981adee6d44811e336156428d8f28eab84
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-26 18:15:28 +0200

    chor :changed scripting files

---
+++ scripts/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /home/ubuntu/nodejs
+cd /home/ubuntu/nodejs
+cd /home/ec2-user
+
+sudo npm install
---
+++ scripts/application_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Stop all servers and start the server as a daemon
+# forever stopall
+# forever start /home/ubuntu/nodejs/app.js
+
+
+node /home/ec2-user/dist/src/server.js
